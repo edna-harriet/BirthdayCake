d8d1e4fa519c184286adfdbf43c60feba05a16e4
edna-harriet <hodima2005@yahoo.com>
2020-12-31 15:23:10 +0300

    Python_Bday_cake

---
+++ song.py
@@ -0,0 +1,18 @@
+'''from playsound import playsound
+import time
+
+@playsound("Best_Happy_Birthday_Song.mp3", wait=10)
+
+def run():
+    for num in reversed(range(10)):
+        print(10/num)
+
+if__name__ == "__main__" :
+    run()
+'''
+from playsound import playsound
+#import vlc
+#import jazzit
+#import time
+
+playsound('/home/harriet/Desktop/Happy birthday - Acappella.MP3')
